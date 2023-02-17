ef29cf209d813dc588eb3f9b60b6f81644c7dd2b
Lwandiso Pohlwana <lwandisopohlwana.com@gmail.com>
2023-02-17 05:06:15 -0800

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints a number untill the base 10 is reached.
+ *
+ * Return: must be 0.
+*/
+
+int main(void)
+{
+	int number;
+
+	fot (number = 0; number <= 10; number++)
+		printf("%d", number);
+
+	printf('\n');
+
+	return (0);
+}
+
