c17b71f285f773b7692ec2970d10b628d0be4059
Lwandiso Pohlwana <lwandisopohlwana.com@gmail.com>
2023-02-18 03:17:58 -0800

    advanced

---
+++ 0x00-hello_world/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: must always be 0.
+ */
+int main(void)
+{
+	int number1, number2;
+
+	for (number1 = 0; number1 < 9; number1++)
+	{
+		for (number2 = number1 + 1; number2 < 10; number2++)
+		{
+			putchar((number1 % 10) + '0');
+			putchar((number2 % 10) + '0');
+
+			if (number1 == 8 && number2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
