24435051fe02ea9a5df10de01d244a5f351d1c72
Lwandiso Pohlwana <lwandisopohlwana.com@gmail.com>
2023-02-21 09:11:22 -0800

    my 8th task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,13 @@
  * Return: The last digit.
  */
 
-int print_last_digit(int n)
+int print_last_digit(int e)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int n;
+
+	n = e % 10;
+
+	if (e < 0)
+		n = -n;
+	return (n);
 }
