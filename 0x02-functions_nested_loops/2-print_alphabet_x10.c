bac68c3a8a13eb2a59c252d58d57c134a5db8dea
Lwandiso Pohlwana <lwandisopohlwana.com@gmail.com>
2023-02-21 07:56:08 -0800

    my third task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - repeats the print_alphabet 10 times.
  *
  * Return: Always 0.
 */
