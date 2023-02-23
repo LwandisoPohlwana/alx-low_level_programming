f3c4879dac71063e564eba4969fbfe36448f0db1
Lwandiso Pohlwana <lwandisopohlwana.com@gmail.com>
2023-02-23 07:44:39 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 	int v;
-	
+
 	v = 1;
 	printf("%d", v);
-		
+
 	for (v = 2; v <= 100; v++)
 	{
 		if (v % 3 == 0 && v % 5 == 0)
@@ -22,15 +22,15 @@ int main(void)
 		}
 		else if (v % 3 == 0)
 		{
-			printf(" Fizz ");
+			printf(" Fizz");
 		}
 		else if (v % 5 == 0)
 		{
-			printf(" Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", v);
+			printf(" %d", v);
 		}
 	}
 	printf("\n");
