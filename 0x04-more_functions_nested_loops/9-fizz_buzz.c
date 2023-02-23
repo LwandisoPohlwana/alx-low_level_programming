4bada50dd33cd297e4036c45756d32c73c1fcb7a
Lwandiso Pohlwana <lwandisopohlwana.com@gmail.com>
2023-02-23 06:42:51 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints a fuzzbuzz program
+ *
+ * Return: is 0.
+*/
+
+int main(void)
+{
+	int v;
+
+	for (v = 1; v <= 100; v++)
+	{
+		if (v % 3 == 0 && v % 5 == 0)
+		{
+			printf("FuzzBuzz ");
+		}
+		else if (v % 3 == 0)
+		{
+			printf("Fuzz ");
+		}
+		else if (v % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", v);
+		}
+	}
+	printf("\n");
+	return (0);
+}
