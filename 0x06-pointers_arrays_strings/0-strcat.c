d1ecfeaaedad02919aaefad4ff2dbe877405580f
Lwandiso Pohlwana <85118790+LwandisoPohlwana@users.noreply.github.com>
2023-03-01 18:25:05 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,6 @@
 char *_strcat(char *dest, char *src)
 {
 	int d, s;
-	int v;
 
 	d = s = 0;
 
@@ -20,13 +19,10 @@ char *_strcat(char *dest, char *src)
 	}
 	while (dest[d] != '\0')
 	{
-		d++;
-	}
-	for (v = 0; v <= src[s]; v++)
-	{
 		dest[d] = src[v];
 		d++;
 		s++;
 	}
+	dest[d] = '\0'
 	return (dest);
 }
