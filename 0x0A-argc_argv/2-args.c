d8a2ed50243c40baac75d6ddf2d9091d85c04a87
Lwandiso Pohlwana <lwandisopohlwana.com@gamil.com>
2023-03-11 02:47:20 -0800

    fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,7 +7,7 @@
 * @argv: array of arguments
 * Return: Always 0 (Success)
 */
- 
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -15,6 +15,6 @@ int main(int argc, char *argv[])
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
-	}		
+	}
 	return (0);
 }
