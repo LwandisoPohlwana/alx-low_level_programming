afadac1c87378332ec57be610691b1c8a6bbcf07
lwandisoPohlwana <lwandisopohlwana.com@gmail.com>
2023-08-17 03:54:50 -0700

    fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -12,7 +12,7 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
 
-	coutnt = 0;
+	count = 0;
 
 	if (h == null)
 		return (count);
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,4 +1,4 @@
-#include "listsh"
+#include "lists.h"
 
 /**
  * sum_dlistint - returns the sum of all the data
